b8a793b833f5b30ccc2395e9020a9a24e6c6db5c
teddy <teddy.juhlin-henricson@bontouch.com>
2022-07-27 17:00:17 +0200

    Add guard to handle when ApiCaller returns nils

--- XKCD Comic Reader/ViewModel.swift
+++ XKCD Comic Reader/ViewModel.swift
@@ -9,23 +9,26 @@ import Foundation
 
 final class ViewModel {
     
-    var xkcdUrl = UrlXKCD()
+//    var xkcdUrl = UrlXKCD()
+    var nextNumber: Int = -1
     
     let apiCaller = ApiCaller()
     
     init() {}
     
-    func fetchXKCD(for number: Int? = nil) async -> (XKCDData?, Data?) {
-        if let comicNumber = number {
-            xkcdUrl.setComicNumber(comicNumber)
-        }
-        let xkcdData: XKCDData? = await apiCaller.fetch(url: xkcdUrl.get())
+    func fetchXKCD() async -> (XKCDData?, Data?) {
+        
+        let xkcdData: XKCDData? = await apiCaller.fetch(url: URL(number: nextNumber))
         guard let imgUrlString = xkcdData?.img else {
             return (nil, nil)
         }
         // TODO: How do we keep track of the latest comic number? Save somewhere? and if so, how? or do we accept that the app will always display the latest comic first?
+        if nextNumber < 0 {
+            nextNumber = xkcdData?.num ?? -1
+//            xkcdUrl.setComicNumber(currentNumber)
+        }
         
-        let imgData: Data? = await apiCaller.fetch(url: imgUrlString)
+        let imgData: Data? = await apiCaller.fetch(url: URL(string: imgUrlString))
         
         return (xkcdData, imgData)
     }
