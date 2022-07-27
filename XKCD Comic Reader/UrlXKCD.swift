46fa389f332cc5ae77e4ee39ee65bbb25738397a
teddy <teddy.juhlin-henricson@bontouch.com>
2022-07-27 13:42:34 +0200

    Add code to easier get the urls for XKCD

--- XKCD Comic Reader/UrlXKCD.swift
+++ XKCD Comic Reader/UrlXKCD.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct UrlXKCD {
+    private var xkcdEnd = "info.0.json"
+    private var xkcdUrl = "https://xkcd.com/"
+    private(set) var xkcdNumber: Int = -1
+    
+    func get(_ latest: Bool = true, for number: Int = -1) -> String {
+        if latest {
+            return xkcdUrl + xkcdEnd
+        }
+        return xkcdUrl + "/" + String(xkcdNumber) + "/" + xkcdEnd
+    }
+    
+    mutating func setComicNumber(_ number: Int?) {
+        guard let number = number else {
+            xkcdNumber = -1
+            return
+        }
+        xkcdNumber = number
+    }
+}
