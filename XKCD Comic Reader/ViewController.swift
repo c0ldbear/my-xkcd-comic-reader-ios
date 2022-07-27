22cdb2849229bbba7b5e2477e73cff30886736c4
teddy <teddy.juhlin-henricson@bontouch.com>
2022-07-27 17:01:00 +0200

    Add handling for returned nils in ViewController

--- XKCD Comic Reader/ViewController.swift
+++ XKCD Comic Reader/ViewController.swift
@@ -14,17 +14,28 @@ class ViewController: UIViewController {
     @IBOutlet private var comicImageView: UIImageView!
     
     let viewModel = ViewModel()
+    var comicNumber: Int = -1 {
+        didSet {
+            viewModel.nextNumber = comicNumber - 1
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         Task.init {
             let (xkcdData, comicImgData) = await viewModel.fetchXKCD()
-            titleLabel.text = xkcdData?.title
-            altTextLabel.text = xkcdData?.alt
-            comicImageView.image = UIImage(data: comicImgData!)
+            
+            guard let xkcdData = xkcdData,
+                  let comicImgData = comicImgData else {
+                return
+            }
+            
+            titleLabel.text = xkcdData.title
+            altTextLabel.text = xkcdData.alt
+            comicNumber = xkcdData.num ?? -1
+            comicImageView.image = UIImage(data: comicImgData)
         }
     }
-
 }
 
